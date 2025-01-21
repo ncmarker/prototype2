6d7f184a37632294d55458b09943840f61022d5d
Gianna Besmonte <gbesmont@usc.edu>
2025-01-20 21:04:30 -0800

    enemy movement + bug fix

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class EnemyController : MonoBehaviour
 {
-    [SerializeField] private GameObject targetGameObject;
+
     [SerializeField] private float moveSpeed;
     [SerializeField] private Rigidbody2D rb;
 
@@ -13,7 +13,19 @@ public class EnemyController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        target = targetGameObject.transform;
+        GameObject[] players = GameObject.FindGameObjectsWithTag("Player");
+        GameObject Player1 = players[0];
+        GameObject Player2 = players[1];
+        
+        float distancePlayer1 = Vector3.Distance(Player1.transform.position, transform.position);
+        float distancePlayer2 = Vector3.Distance(Player2.transform.position, transform.position);
+        //Debug.Log(distancePlayer1 + " " + distancePlayer2);
+        if (distancePlayer1 <= distancePlayer2){
+            target = Player1.transform;
+        }
+        else {
+            target = Player2.transform;
+        }
     }
 
     // Update is called once per frame
